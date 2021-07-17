dc5dc523a4bcbdac66fe0a2454ffc456cb8b94aa
Ade Priyanto <mr.adepriyanto@gmail.com>
2021-07-18 03:33:02 +0700

    Initial Commit

--- AdminLte3Asset.php
+++ AdminLte3Asset.php
@@ -11,10 +11,13 @@ class AdminLte3Asset extends BaseAdminLteAsset {
 
     public $sourcePath = '@vendor/almasaeed2010/adminlte/dist';
     public $css = [
-        'css/adminlte.css',
+        'css/adminlte.min.css',
     ];
     public $js = [
-        'js/adminlte.js',
+        'js/adminlte.min.js',
+        'js/pages/dashboard.js',
+        'js/pages/dashboard2.js',
+        'js/pages/dashboard3.js',
         'js/demo.js',
     ];
     public $depends = [
